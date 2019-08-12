505c8cd6b679e295bf43a267e4d6c649cb0582cd
jisung <jisung.smart@gmail.com>
2019-08-12 16:59:18 +0900

    If user long press the screen at that moment the annotation is added at that point

--- MapTest/ViewController.swift
+++ MapTest/ViewController.swift
@@ -42,6 +42,29 @@ class ViewController: UIViewController, MKMapViewDelegate {
         annotation.coordinate = coordinate
         
         map.addAnnotation(annotation)
+        
+        let uiLongpress = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longpressed(gestureRecongnizer:)))
+        
+        uiLongpress.minimumPressDuration = 2
+        
+        map.addGestureRecognizer(uiLongpress)
+    }
+    
+    @objc func longpressed(gestureRecongnizer : UIGestureRecognizer){
+    
+        let touchPoint = gestureRecongnizer.location(in: self.map)
+        
+        let coordinate = map.convert(touchPoint, toCoordinateFrom: self.map)
+        
+        let annotation = MKPointAnnotation()
+
+        annotation.coordinate = coordinate
+        
+        annotation.title = "New Place"
+        
+        annotation.subtitle = "Here is the new place"
+
+        map.addAnnotation(annotation)
     }
 
 
