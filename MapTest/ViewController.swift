6cb261d80e5450d38a03cb1401cc93bb67d61ffe
jisung <jisung.smart@gmail.com>
2019-08-12 16:30:58 +0900

    Initial Commit

---
+++ MapTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapTest
+//
+//  Created by The book Air on 12/08/2019.
+//  Copyright © 2019 jisung. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var map: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
